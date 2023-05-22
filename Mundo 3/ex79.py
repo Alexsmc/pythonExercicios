c36bb567343ba589d303dd480689fdcc94db5d52
Alexsmc <alex.smc@hotmail.com>
2023-05-22 18:23:56 -0300

    exercicio 79 corrigido

--- Mundo 3/ex79.py
+++ Mundo 3/ex79.py
@@ -2,12 +2,11 @@ lista = list()
 repeat = 'Y'
 while repeat in 'yY':
     num = int(input('Digite um numero: '))
-    for p, v in enumerate(lista):
-        if v == num:
-            print('Numero já adicionado')
-        else:
-            lista.append(num)
-
+    if num not in lista:
+        print('Numero adicionado')
+        lista.append(num)
+    else:
+        print('Numero duplicado. Não vou adicionar')
     repeat = str(input('Quer continuar? [Y/N] '))
     while repeat not in 'yYnN':
         repeat = str(input('Quer continuar? [Y/N] '))
