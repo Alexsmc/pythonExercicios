e20da78842b378244bd9bf19d84c2e088dad4ccc
Alexsmc <alex.smc@hotmail.com>
2023-05-23 14:12:08 -0300

    85 corrigido

---
+++ Mundo 3/ex85.py
@@ -0,0 +1,12 @@
+numeros = [[],[]]
+valor = 0
+for i in range(7):
+    valor = int(input(f'Digite o {i+1}º valor: '))
+    if valor %2 == 0:
+        numeros[0].append(valor)
+    else:
+        numeros[1].append(valor)
+numeros[0].sort()
+numeros[1].sort()
+print(f'Os numeros paares foram os: {numeros[0]}')
+print(f'Os valores ímpares digitados foram {numeros[1]}')
