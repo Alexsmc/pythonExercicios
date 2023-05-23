cd1df0509fceb3607e894cab6d2f29f17519d20b
Alexsmc <alex.smc@hotmail.com>
2023-05-22 21:34:10 -0300

    exercicio 81 corrigido

--- Mundo 3/ex81.py
+++ Mundo 3/ex81.py
@@ -1,7 +1,15 @@
 valores = []
-while True
+while True:
     valores.append(int(input('Digite um valor')))
-    r = 'y'
-    if r not in 'nN':
+    r = 'z'
+    while r not in 'sSnN':
         r = input('Quer continuar? [S/N]').strip().upper()
-
+    if r in 'nN':
+        break
+print(f'Você digitou {len(valores)} Elementos')
+valores.sort(reverse=True)
+print(f'Os valores em ordem decrescente são: {valores}')
+if 5 in valores:
+    print('O Número 5 está na lista')
+else:
+    print('Você não digitou o N° 5')
