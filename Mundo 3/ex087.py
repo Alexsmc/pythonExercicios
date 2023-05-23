d445c1c458bda17fac87fa77b2d2d3b097178137
Alexsmc <alex.smc@hotmail.com>
2023-05-23 15:25:03 -0300

    87 corrigido

--- Mundo 3/ex087.py
+++ Mundo 3/ex087.py
@@ -1,21 +1,31 @@
+#Declaração de variáveis
 matriz = [[0,0,0],[0,0,0],[0,0,0]]
 maior2l = totalpar = totalcol3 = 0
+
+#Preencher a Matriz
 for l in range(3):
     for c in range(3):
         matriz[l][c] = int(input(f'Digite o valor para [{l},{c}]: '))
 print('='*20)
+
+#Imprimir a matriz e somar os pares
 for l in range(3):
     for c in range(3):
         if matriz[l][c]%2 == 0:
             totalpar += matriz[l][c]
         print(f'[{matriz[l][c]:^3}]', end='')
     print()
+
+#Soma dos pares
 for l in range(3):
     totalcol3 += matriz[l][2]
+
+#Numero maior
 for c in range(3):
-    if matriz[1][c] > maior2l:
+    if c == 0 or matriz[1][c] > maior2l:
         maior2l = matriz[1][c]
 
+#Imprimindo as respostas
 print(f'A soma de todos os valores pares é: {totalpar}')
 print(f'A soma dos valores da 3º coluna é: {totalcol3}')
 print(f'O maior valor da segunda linha é: {maior2l}')
