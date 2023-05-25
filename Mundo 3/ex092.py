a3cce1f3ff2dadd0ef00d2a24a24f5c3a01eb59b
Alexsmc <alex.smc@hotmail.com>
2023-05-25 17:18:33 -0300

    exercício 91 feito

---
+++ Mundo 3/ex092.py
@@ -0,0 +1,21 @@
+import datetime
+trabalhador = list()
+dados = dict()
+while True:
+    dados['Nome: '] = str(input('Nome: ')).strip().title()
+    idade = int(input("Ano de nascimento: "))
+    dados['Idade: '] = datetime.date.today().year - idade
+    dados['CTPS: '] = int(input('Carteira de Trabalho: (0 não tem): '))
+    if dados['CTPS: '] == 0:
+        trabalhador.append(dados.copy())
+        break
+    dados['Ano contratação: '] = int(input(('Ano de Contratação: ')))
+    dados['Salário: R$ '] = float(input('Salário: R$'))
+    dados['Aposentadoria em: '] = dados['Ano contratação: ']+35
+    trabalhador.append(dados.copy())
+    break
+
+for e in trabalhador:
+    for k, v in e.items():
+        print(f'{k} {v}')
+
