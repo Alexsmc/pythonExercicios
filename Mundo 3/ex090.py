29e4a26dbd4fca1034e5b5d5129c86527d5e57e8
Alexsmc <alex.smc@hotmail.com>
2023-06-15 14:32:42 -0300

    Atualização do exercício 90. praticando funções

--- Mundo 3/ex090.py
+++ Mundo 3/ex090.py
@@ -3,7 +3,20 @@ aluno['nome'] = str(input('Nome: '))
 aluno['média'] = float(input(f'Média de {aluno["nome"]}: '))
 print(f'O aluno: {aluno["nome"]}')
 print(f'Teve média: {aluno["média"]}')
-if aluno['média'] < 7:
-    print('Situação: REPROVADO')
-else:
-    print('Situação: APROVADO')
+
+def situar(media):
+    if media < 7:
+        return 'REPROVADO'
+    elif 5 <= media < 7:
+        return 'RECUPERAÇÃO'
+    else:
+        return 'REPROVADO'
+    pass
+
+
+aluno['situação'] = situar(aluno["média"])
+
+def imprimir_aluno():
+    for k, v in aluno.items():
+        print(f'   - {k} é igual {v}')
+imprimir_aluno()
