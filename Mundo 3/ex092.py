d67df83eca1bc5662887dbb6457548bce7247d5f
Alexsmc <alex.smc@hotmail.com>
2023-06-15 15:09:41 -0300

    Atualização do exercício 92. praticando funções

--- Mundo 3/ex092.py
+++ Mundo 3/ex092.py
@@ -9,13 +9,12 @@ while True:
     if dados['CTPS: '] == 0:
         trabalhador.append(dados.copy())
         break
-    dados['Ano contratação: '] = int(input(('Ano de Contratação: ')))
+    dados['Ano contratação: '] = int(input('Ano de Contratação: '))
     dados['Salário: R$ '] = float(input('Salário: R$'))
-    dados['Aposentadoria em: '] = dados['Ano contratação: ']+35
+    dados['Aposentadoria com: '] = str(f'{dados["Idade: "]+35} anos.')
     trabalhador.append(dados.copy())
     break
 
 for e in trabalhador:
     for k, v in e.items():
-        print(f'{k} {v}')
-
+        print(f'{k}{v}')
