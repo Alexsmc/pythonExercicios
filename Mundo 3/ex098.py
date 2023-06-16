49186cc7bbe95212ecb028b50910944eb1c2261b
Alexsmc <alex.smc@hotmail.com>
2023-06-16 10:43:50 -0300

    Excício 98, corrigido e atualizado

--- Mundo 3/ex098.py
+++ Mundo 3/ex098.py
@@ -1,10 +1,12 @@
 from time import sleep
+
+
 def contador(inicio, fim, passo):
-    if passo <0:
+    if passo < 0:
         passo *= -1
     elif passo == 0:
         passo = 1
-
+    print(f'Mostrando a contagem de {inicio} até {fim} de {passo} em {passo}')
     if inicio < fim:
         while inicio <= fim:
             print(inicio, end=', ')
