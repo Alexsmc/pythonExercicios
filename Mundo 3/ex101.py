265a17009ca2beed796d2d1ba9209e6be62f1ba9
Alexsmc <alex.smc@hotmail.com>
2023-06-19 15:39:36 -0300

    Excício 101: criado e respondido

---
+++ Mundo 3/ex101.py
@@ -0,0 +1,25 @@
+import datetime
+
+
+def calc_idade(ano_nasc):
+    """
+    Função que calcula a idade atual do individuo
+    :param ano_nasc: recebe o ano que o usuário nasceu
+    :return: retorna a idade atual do usuário
+    """
+    idade = datetime.date.today().year - ano_nasc
+    return idade
+
+
+def voto(idade):
+    if idade < 16:
+        print(f'com {idade} anos: VOTO NEGADO')
+    elif 18 <= idade <= 64:
+        print(f'com {idade} anos: VOTO OBRIGATÓRIO')
+    else:
+        print(f'com {idade} ano: VOTO OPCIONAL')
+
+
+# Programa Principal
+idade_atual = calc_idade(int(input('Em que ano você nasceu? ')))
+voto(idade_atual)
