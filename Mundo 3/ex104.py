a67b36ace7c6ba0d7098a478cf0ae17b39bf28f5
Alexsmc <alex.smc@hotmail.com>
2023-07-06 15:05:21 -0300

    Exercício 104,resolvido e corrigido

--- Mundo 3/ex104.py
+++ Mundo 3/ex104.py
@@ -1,2 +1,24 @@
-def leiaInt(string="Digite um número: "):
-    n = (input(string))
+def leiaInt(msg):
+    '''
+    Função para validar se o dado é um numero inteiro
+    :param n: pega o numero digitado pelo usuário
+    :return: retona o numero se verdadeiro
+    '''
+
+    ok = False
+    valor = 0
+    while True:
+        n = str(input(msg))
+        if n.isnumeric():
+            valor = int(n)
+            ok = True
+        else:
+            print('\033[0;31mERRO! Digite um numero inteiro válido.\033[m')
+        if ok:
+            break
+    return valor
+
+
+# main
+n = leiaInt('Digite um número: ')
+print(f'Você acabou de digitar o numero {n}')
