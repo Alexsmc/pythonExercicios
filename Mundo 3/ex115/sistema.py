b62b8bb3de28b2fcf276a78f562c912049855c00
Alexsmc <alex.smc@hotmail.com>
2023-07-11 21:13:34 -0300

    Exercício 115b ,resolvido e corrigido. (Arquivos em Python)

--- Mundo 3/ex115/sistema.py
+++ Mundo 3/ex115/sistema.py
@@ -1,11 +1,21 @@
-from lib import interface
+from lib import interface, arquivo
+
+arq = 'lista_de_nomes.txt'
+
+if arquivo.arquivoExiste(arq):
+    print('Arquivo encontrado com sucesso!')
+else:
+    print('Arquivo não encontrado')
+    arquivo.criarArquivo(arq)
 
 while True:
-    resposta = interface.menu(['Ver pessoa cadastrada', 'Cadastrar nova pessoa', 'Sair do sistema'])
+    resposta = interface.menu(['Ver pessoa cadastrada', 'Cadastrar nova pessoa', 'Cadastrar nova pessoa'])
     if resposta == 1:
-        interface.cabeçalho('opc 1')
+        # Opção de listar o conteúdo de um arquivo!
+        arquivo.lerArquivo(arq)
+        interface.cabeçalho('Ver pessoa cadastrada')
     elif resposta == 2:
-        interface.cabeçalho('opc 2')
+        interface.cabeçalho('Cadastrar nova pessoa')
     elif resposta == 3:
         interface.cabeçalho('Até logo!')
         break
